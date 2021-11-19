423e1192be0d33ed6039b36af33e1c550e7aab07
Claiton Neri <claitondidone@gmail.com>
2021-11-19 01:24:55 -0300

    dev: add code formatter

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World.'})
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World.' }));
 
-app.listen(3333, () => console.log("API Started."))
+app.listen(3333, () => console.log('API Started.'));
