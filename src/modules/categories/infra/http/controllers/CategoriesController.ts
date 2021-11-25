2791a7c3395ea398a8befa69dd7dcfbb51d49a45
Claiton Neri <claitondidone@gmail.com>
2021-11-24 23:37:59 -0300

    chore: create controller to Category

--- src/modules/categories/infra/http/controllers/CategoriesController.ts
+++ src/modules/categories/infra/http/controllers/CategoriesController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
+import { container } from 'tsyringe';
+
+import CreateCategoryService from '@modules/categories/services/CreateCategoryService';
 
 class CategoriesController {
-  async create(_: Request, response: Response): Promise<Response> {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, description } = request.body;
+
+    const createCategory = container.resolve(CreateCategoryService);
+
     return response.json({ message: "ok" });
   }
 }
