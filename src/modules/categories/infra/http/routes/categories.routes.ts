96f7289169424afffb48919ab60941760b0331ee
Claiton Neri <claitondidone@gmail.com>
2021-11-27 22:25:15 -0300

    chore: add route get for list categories

--- src/modules/categories/infra/http/routes/categories.routes.ts
+++ src/modules/categories/infra/http/routes/categories.routes.ts
@@ -5,6 +5,7 @@ import CategoriesController from "../controllers/CategoriesController";
 const categoriesRouter = Router();
 const categoriesController = new CategoriesController();
 
+categoriesRouter.get('/', categoriesController.index);
 categoriesRouter.post("/", categoriesController.create);
 
 export default categoriesRouter;
